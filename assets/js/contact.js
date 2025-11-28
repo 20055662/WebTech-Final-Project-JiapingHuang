dd92981819e7c6d5ed9914dbbac9d64582b60c5f
20055662 <20055662@tafe.wa.edu.au>
2025-11-28 18:33:53 +0800

    feat(contact): crate contact page with tailwindCSS and JavaScript

---
+++ assets/js/contact.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const form = document.getElementById("contact-form");
+    const nameField = document.getElementById("name");
+    const emailField = document.getElementById("email");
+    const messageField = document.getElementById("message");
+
+    const nameError = document.getElementById("name-error");
+    const emailError = document.getElementById("email-error");
+    const messageError = document.getElementById("message-error");
+
+    //Regex Patterns
+    const nameRegex = /^[A-Za-z\s]{2,50}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    //Validation Funtion
+    function ValidateForm(e){
+        let valid =true;
+
+        if (!nameRegex.test(nameField.value.trim())){
+            nameError.classList.remove("hidden");
+            valid = false;
+        } else{
+            nameError.classList.add("hidden");
+        }
+
+        if (!emailRegex.test(emailField.value.trim())){
+            emailError.classList.remove("hidden");
+            valid = false;
+        } else{
+            emailError.classList.add("hidden");
+        }
+
+        if(messageField.value.trim().length <10){
+            messageError.classList.remove("hidden");
+            valid = false;
+        } else{
+            messageError.classList.add("hidden");
+        }
+
+        if(!valid){
+            e.preventDefault();
+        }
+    }
+
+    form.addEventListener("submit", ValidateForm);
+
+})
